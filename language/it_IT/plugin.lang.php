1ee6330767c600d7e04c0b2743a5e115e4679050
riopwg <riopwg@piwigo.org>
2009-07-01 21:11:20 +0000

    Langue IT
    git-svn-id: http://piwigo.org/svn/extensions/typetags@3492 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+
+$lang['typetags_error'] = 'Dovete inserire tutti campi (nome e colore).';
+$lang['typetags_saved'] = 'TypeTag registrato.';
+$lang['typetags_suppr'] = 'TypeTags cancellati (%d).';
+$lang['typetag_suppr'] = 'TypeTag cancellato.';
+$lang['typetags_already exists'] = 'Questo TypeTag esiste già.';
+$lang['Edit selected typetags'] = 'Modificare i TypeTags selezionati.';
+$lang['Delete selected typetags'] = 'Cancellare i TypeTags selezionati.';
+$lang['Edit typetags'] = 'Modificare i TypeTags';
+$lang['New color'] = 'Nuovo colore';
+$lang['Create a Typetag'] = 'Creare un TypeTag';
+$lang['New TypeTag'] = 'Nome del TypeTag';
+$lang['ColorTypeTag'] = 'Colore del TypeTag';
+$lang['TypeTag selection'] = 'Modificare - Cancellare i TypeTags';
+$lang['TypeTag association'] = 'Associare i TypeTags';
+$lang['TypeTag dissociation'] = 'Dissociare i TypeTags';
+$lang['existing_typetag_list'] = 'TypeTag :';
+$lang['associate'] = 'Associare';
+$lang['dissociate'] = 'Dissociare';
+$lang['typetags_associated'] = 'TypeTag associato.';
+$lang['typetags_dissociated'] = 'TypeTag dissociato.';
+$lang['choose_typetag'] = ' Cegliere un TypeTag da associare ';
+$lang['error_associate'] = ' Dovete scegliere almeno un Tag E un TypeTag ';
+$lang['error_dissociate'] = ' Dovete scegliere almeno un Tag da dissociare ';
+
+$lang['Edited TypeTag'] = 'Typetag modificato';
+$lang['New name'] = 'Nuovo nome';
+$lang['Modify'] = 'Modificare';
+$lang['You should select only one Typetag to edit.'] = 'Non potete modificare più di un Typetag alla volta.';
+
+?>
