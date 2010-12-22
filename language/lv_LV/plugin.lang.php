25a638f19935225f4aa9369dd39d6787e8f94cc3
ddtddt <ddtddt@piwigo.org>
2010-12-22 18:01:59 +0000

    [extensions] - typetags -  add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/typetags@8230 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+
+$lang['typetags_error'] = 'Jums jāinformē visus laukus (nosaukums un krāsa).';
+$lang['typetags_saved'] = 'TypeTags saglabāts.';
+$lang['typetags_suppr'] = 'TypeTagi dzēsti.';
+$lang['typetag_suppr'] = 'TypeTags dzēsts.';
+$lang['typetags_already exists'] = 'Šis TypeTags jau eksistē.';
+$lang['Edit selected typetags'] = 'Rediģēt atlasītos TypeTagus.';
+$lang['Delete selected typetags'] = 'Dzēst atlasītos TypeTagus.';
+$lang['Edit typetags'] = 'Rediģēt TypeTagus';
+$lang['New color'] = 'Jauna krāsa';
+$lang['Create a Typetag'] = 'Izveidot TypeTagu';
+$lang['New TypeTag'] = 'TypeTaga nosaukums';
+$lang['ColorTypeTag'] = 'TypeTaga krāsa';
+$lang['TypeTag selection'] = 'Rediģēt - Dzēst TypeTagus';
+$lang['TypeTag association'] = 'Sasaistīt TypeTagus';
+$lang['TypeTag dissociation'] = 'Atsaistīt TypeTagus';
+$lang['existing_typetag_list'] = 'TypeTags, ko sasaistīt :';
+$lang['associate'] = 'Sasaistīt';
+$lang['dissociate'] = 'Atsaistīt';
+$lang['typetags_associated'] = 'TypeTags sasaistīts.';
+$lang['typetags_dissociated'] = 'TypeTags atsaistīts.';
+$lang['choose_typetag'] = ' Izvēlēties TypeTagu, ko sasaistīt ';
+$lang['error_associate'] = ' Jums jāizvēlas iezīme Tags UND TypeTags ';
+$lang['error_dissociate'] = ' Jums jāizvēlas iezīme/s Tag(s), lai atsaistītu ';
+
+$lang['Edited TypeTag'] = 'Rediģētais Typetags';
+$lang['New name'] = 'Jauns vārds';
+$lang['Modify'] = 'Modificēt';
+$lang['You should select only one Typetag to edit.'] = 'Jums jāizvēlas tikai viens Typetags, ko rediģēt.';
+
+?>
