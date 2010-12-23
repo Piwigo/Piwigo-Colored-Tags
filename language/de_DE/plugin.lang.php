c41fa81cf38449c239297c712d43d5f5a70ab10b
ddtddt <ddtddt@piwigo.org>
2010-12-23 21:01:00 +0000

    [extensions] - typetags -  add de_DE thanks to sugar888
    git-svn-id: http://piwigo.org/svn/extensions/typetags@8268 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+
+$lang['typetags_error'] = 'Alle Felder müssen ausgefüllt werden (Name und Farbe).';
+$lang['typetags_saved'] = 'TypeTag gespeichert.';
+$lang['typetags_suppr'] = 'TypeTags gelöscht.';
+$lang['typetag_suppr'] = 'TypeTag gelöscht.';
+$lang['typetags_already exists'] = 'Dieser TypeTag existiert bereits.';
+$lang['Edit selected typetags'] = 'Die ausgewählten TypeTags bearbeiten.';
+$lang['Delete selected typetags'] = 'Die ausgewählten TypeTags löschen.';
+$lang['Edit typetags'] = 'TypeTags bearbeiten';
+$lang['New color'] = 'Neue Farbe';
+$lang['Create a Typetag'] = 'Einen TypeTag erstellen';
+$lang['New TypeTag'] = 'Name des TypeTags';
+$lang['ColorTypeTag'] = 'Farbe des TypeTags';
+$lang['TypeTag selection'] = 'TypeTags bearbeiten - löschen';
+$lang['TypeTag association'] = 'TypeTags assoziieren';
+$lang['TypeTag dissociation'] = 'TypeTags dissoziieren';
+$lang['existing_typetag_list'] = 'Zu assoziierende TypeTags :';
+$lang['associate'] = 'Assoziieren';
+$lang['dissociate'] = 'Dissoziieren';
+$lang['typetags_associated'] = 'TypeTag wurde assoziiert.';
+$lang['typetags_dissociated'] = 'TypeTag wurde dissoziiert.';
+$lang['choose_typetag'] = ' TypeTag auswählen, welcher assoziiert werden soll ';
+$lang['error_associate'] = ' Sie müssen einen oder mehreren Tags und einen Typetag auswählen ';
+$lang['error_dissociate'] = ' Sie müssen die zu dissoziierenden Tags auswählen ';
+
+$lang['Edited TypeTag'] = 'Typetag bearbeiten';
+$lang['New name'] = 'Neuer Name';
+$lang['Modify'] = 'Modifizieren';
+$lang['You should select only one Typetag to edit.'] = 'Sie sollten nur einen zu bearbeitenden Typetag auswählen.';
+
+
+?>
