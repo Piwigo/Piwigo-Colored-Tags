b44a174ed61e26e2d8bc5731218142ddc4bb2041
ddtddt <ddtddt@piwigo.org>
2011-02-12 12:14:20 +0000

    [extensions] - typetags - add no_NO (Norwegian) thanks to atlew
    git-svn-id: http://piwigo.org/svn/extensions/typetags@9196 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/no_NO/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+
+$lang['typetags_error'] = 'Du må inkludere alle felter (navn og farge).';
+$lang['typetags_saved'] = 'TypeTag lagre.';
+$lang['typetags_suppr'] = 'TypeTager slettet.';
+$lang['typetag_suppr'] = 'TypeTag slettet.';
+$lang['typetags_already exists'] = 'Denne TypeTagen eksisterer allerede.';
+$lang['Edit selected typetags'] = 'Rediger valgte TypeTager.';
+$lang['Delete selected typetags'] = 'Slett valgte TypeTager.';
+$lang['Edit typetags'] = 'Rediger TypeTags';
+$lang['New color'] = 'Ny farge';
+$lang['Create a Typetag'] = 'Lag en TypeTag';
+$lang['New TypeTag'] = 'Navn på TypeTag';
+$lang['ColorTypeTag'] = 'Farge på TypeTag';
+$lang['TypeTag selection'] = 'Rediger - Slett TypeTager';
+$lang['TypeTag association'] = 'Assosier TypeTager';
+$lang['TypeTag dissociation'] = 'Dissossier TypeTager';
+$lang['existing_typetag_list'] = 'TypeTag å assosiere med :';
+$lang['associate'] = 'Assossiere';
+$lang['dissociate'] = 'Dissossiere';
+$lang['typetags_associated'] = 'TypeTag assossiert.';
+$lang['typetags_dissociated'] = 'TypeTag dissossiert.';
+$lang['choose_typetag'] = ' Velg TypeTag a assossiere med ';
+$lang['error_associate'] = ' Du må velge Tagger OG en TypeTag ';
+$lang['error_dissociate'] = ' Du må velge Tagger å dissosiere med ';
+
+$lang['Edited TypeTag'] = 'Rediger Typetag';
+$lang['New name'] = 'Nytt navn';
+$lang['Modify'] = 'Endre';
+$lang['You should select only one Typetag to edit.'] = 'Du bør velge kun en Typetag å redigere.';
+
+?>
