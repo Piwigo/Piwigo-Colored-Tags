1745c7fa595d28bab8ef4f758bde163c86617c03
dodo <dodo@piwigo.org>
2011-03-23 15:28:19 +0000

    git-svn-id: http://piwigo.org/svn/extensions/typetags@9841 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,32 @@
+<?php
+
+$lang['typetags_error'] = 'Môžete meniť všetky údaje (názov a farbu).';
+$lang['typetags_saved'] = 'Typ kľúčového slova uložený.';
+$lang['typetags_suppr'] = 'Typ kľúčových slov vymazaný.';
+$lang['typetag_suppr'] = 'Typ kľúčového slova vymazaný.';
+$lang['typetags_already exists'] = 'Tento typ kľúčového slova už existuje.';
+$lang['Edit selected typetags'] = 'Upraviť označené kľúčového slová.';
+$lang['Delete selected typetags'] = 'Vymazať označené kľúčového slová.';
+$lang['Edit typetags'] = 'Upraviť kľúčového slová';
+$lang['New color'] = 'Nová farba';
+$lang['Create a Typetag'] = 'Vytvoriť kľúčové slovo';
+$lang['New TypeTag'] = 'Názov kľúčového slova';
+$lang['ColorTypeTag'] = 'Fraba kľúčového slova';
+$lang['TypeTag selection'] = 'Upraviť - vymazať kľúčového slová';
+$lang['TypeTag association'] = 'Spojené kľúčového slová';
+$lang['TypeTag dissociation'] = 'Rozdelené kľúčového slová';
+$lang['existing_typetag_list'] = 'Kľúčové slovo na spojenie:';
+$lang['associate'] = 'Spojené';
+$lang['dissociate'] = 'Rozdelené';
+$lang['typetags_associated'] = 'Kľúčové slovo spojené.';
+$lang['typetags_dissociated'] = 'Kľúčové slovo rozdelené.';
+$lang['choose_typetag'] = ' Vybrať kľúčové slovo na spojenie ';
+$lang['error_associate'] = ' Musíte vybrať kľúčové slovo(á) A a typ kľúčového slova ';
+$lang['error_dissociate'] = ' Musíte vybrať kľúčové slovo(á) na rozdelenie ';
+
+$lang['Edited TypeTag'] = 'Upravený typ kľúčového slova';
+$lang['New name'] = 'Nový názov';
+$lang['Modify'] = 'Zmeniť';
+$lang['You should select only one Typetag to edit.'] = 'Mali by ste vybrať len jeden typ kľúčového slova na úpravu.';
+
+?>
