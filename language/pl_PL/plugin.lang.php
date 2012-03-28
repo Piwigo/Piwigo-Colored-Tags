79fb899611f53b2817c234ea7634b5e442a5fac3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-28 06:46:20 +0000

    [TypeT@gs] Add pl_PL, thanks to : Greybrow
    git-svn-id: http://piwigo.org/svn/extensions/typetags@13780 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pl_PL/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All associations have been removed'] = 'Usunięto wszystkie powiązania';
+$lang['Color TypeTag'] = 'Kolor TypeTag';
+$lang['Create a Typetag'] = 'Stwórz TypeTag';
+$lang['Delete all associations'] = 'Skasuj wszystkie powiązania';
+$lang['Edit typetag'] = 'Edytuj TypeTag';
+$lang['Edited TypeTag'] = 'Zmieniono TypeTag';
+$lang['Modify'] = 'Zmień';
+$lang['New color'] = 'Nowy kolor';
+$lang['New name'] = 'Nowa nazwa';
+$lang['New TypeTag'] = 'Nazwa TypeTag';
+$lang['TypeTag association'] = 'Powiązania - usuń powiązania TypeTag';
+$lang['TypeTag selection'] = 'Edycja - Usuwanie TypeTag';
+$lang['typetags_associated'] = 'Powiązano TypeTag';
+$lang['typetag_already_exists'] = 'Ten TypeTag już istnieje';
+$lang['typetag_error'] = 'Należy uzupełnić wszystkie pola (nazwę i kolor)';
+$lang['typetag_saved'] = 'Zapisano TypeTag';
+$lang['typetag_suppr'] = 'Skasowano TypeTag';
+$lang['typetag_unknown'] = 'Nieznany TypeTag';
+?>
