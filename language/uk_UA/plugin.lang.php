c45f323e70ea1f50419d481efcdd70a5a1e70003
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-03-29 05:36:25 +0000

    [TypeT@gs] Add uk_UA, thanks to : animan
    git-svn-id: http://piwigo.org/svn/extensions/typetags@13794 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All associations have been removed'] = 'Всі асоціації були видалені';
+$lang['Color TypeTag'] = 'Колір TypeTag';
+$lang['Create a Typetag'] = 'Створити TypeTag';
+$lang['Delete all associations'] = 'Видалити всі асоціації';
+$lang['Edit typetag'] = 'Редагувати TypeTag';
+$lang['Edited TypeTag'] = 'TypeTags відредагований';
+$lang['Modify'] = 'Змінити';
+$lang['New color'] = 'Новий колір';
+$lang['New name'] = 'Нове ім\'я';
+$lang['New TypeTag'] = 'Ім\'я TypeTag';
+$lang['TypeTag association'] = 'Асоціювати - Дисоціювати TypeTags';
+$lang['TypeTag selection'] = 'Редагування - Видалити TypeTags';
+$lang['typetags_associated'] = 'TypeTags, пов\'язані';
+$lang['typetag_already_exists'] = 'Це TypeTag вже існує';
+$lang['typetag_error'] = 'Ви повинні заповнити всі поля (ім\'я та колір)';
+$lang['typetag_saved'] = 'TypeTag збережено';
+$lang['typetag_suppr'] = 'TypeTag видалено';
+$lang['typetag_unknown'] = 'Невідомий TypeTag';
+?>
