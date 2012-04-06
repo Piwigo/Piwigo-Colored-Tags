b69ea89a98bd138188ed3762e2158bd5264b6ce1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-06 11:24:58 +0000

    [TypeT@gs] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/typetags@13934 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -19,11 +19,11 @@ $lang['Delete all associations'] = 'Delete all associations';
 
 /* messages */
 $lang['typetag_already_exists'] = 'Tento typ kľúčového slova už existuje';
-$lang['typetags_saved'] = 'Typ kľúčového slova uložený';
-$lang['typetags_suppr'] = 'Typ kľúčových slov vymazaný';
 $lang['typetag_unknown'] = 'Unknown TypeTag';
 $lang['typetag_error'] = 'Môžete meniť všetky údaje (názov a farbu)';
 $lang['All associations have been removed'] = 'All associations have been removed';
 $lang['typetags_associated'] = 'Kľúčové slovo spojené';
 
+$lang['typetag_saved'] = 'Napísané kľúčové slová uložené';
+$lang['typetag_suppr'] = 'Napísané kľúčové slová vymazané';
 ?>
