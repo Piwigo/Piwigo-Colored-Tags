40179030c5a75a1f869be1b1b759b8ce8f8bbbc3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-26 15:16:36 +0000

    [typetgs] Add ru_RU, thanks to : nadusha
    git-svn-id: http://piwigo.org/svn/extensions/typetags@14439 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All associations have been removed'] = 'Все связи удалены';
+$lang['Color TypeTag'] = 'Цвет типа тегов';
+$lang['Create a Typetag'] = 'Создать тип тегов';
+$lang['Delete all associations'] = 'Удалить все связи';
+$lang['Edit typetag'] = 'Редактировать тип тегов';
+$lang['Edited TypeTag'] = 'Тип тега изменен';
+$lang['Modify'] = 'Изменить';
+$lang['New TypeTag'] = 'Название типа тегов';
+$lang['New color'] = 'Новый цвет';
+$lang['New name'] = 'Новое название';
+$lang['TypeTag association'] = 'Связать - отделить типы тегов';
+$lang['TypeTag selection'] = 'Редактировать - Удалить типы тегов';
+$lang['typetag_already_exists'] = 'Этот тип тегов уже есть';
+$lang['typetag_error'] = 'Нужно заполнить все поля (название и цвет)';
+$lang['typetag_saved'] = 'Тип тегов сохранен';
+$lang['typetag_suppr'] = 'Тип тегов удален';
+$lang['typetag_unknown'] = 'Неизвестный тип тегов';
+$lang['typetags_associated'] = 'Связи тегов с типами тегов';
+?>
