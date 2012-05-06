31fdc26e2e912b80e6ea09d6dae31c5d1aeb4f6a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-06 23:07:33 +0000

    [typetgs] Update nl_NL, thanks to : ruud
    git-svn-id: http://piwigo.org/svn/extensions/typetags@14812 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All associations have been removed'] = 'Alle toekenningen zijn verwijderd';
+$lang['Color TypeTag'] = 'TypeTag kleur';
+$lang['Create a Typetag'] = 'Maak een TypeTag';
+$lang['Delete all associations'] = 'Verwijder alle toekenningen';
+$lang['Edit typetag'] = 'Wijzig TypeTag';
+$lang['Edited TypeTag'] = 'TypeTag gewijzigd';
+$lang['Modify'] = 'Wijzig';
+$lang['New TypeTag'] = 'TypeTag naam';
+$lang['New color'] = 'Nieuwe kleur';
+$lang['New name'] = 'Nieuwe naam';
+$lang['TypeTag association'] = 'Toekennen - Verwijderen van TypeTags';
+$lang['TypeTag selection'] = 'Wijzig - Verwijder TypeTags';
+$lang['typetag_already_exists'] = 'Deze TypeTag bestaat al';
+$lang['typetag_error'] = 'Je moet alle velden invullen (naam en kleur)';
+$lang['typetag_saved'] = 'TypeTag opgeslagen';
+$lang['typetag_suppr'] = 'TypeTag verwijderd';
+$lang['typetag_unknown'] = 'Onbekende TypeTag';
+$lang['typetags_associated'] = 'TypeTags toegekend';
+?>
