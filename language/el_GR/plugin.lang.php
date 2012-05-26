3f7c1fa14abb94b5a9adb1e193da9c73f4483659
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-26 09:53:41 +0000

    [typetgs] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/typetags@15317 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All associations have been removed'] = 'Όλες οι διασυνδέσεις έχουν αφαιρεθεί';
+$lang['Color TypeTag'] = 'TypeTag χρώμα';
+$lang['Create a Typetag'] = 'Δημιουργία TypeTag';
+$lang['Delete all associations'] = 'Διαγραφή όλων των διασυνδέσεων';
+$lang['Display colored tags'] = 'Εμφάνιση χρωματισμένες ετικέττες';
+$lang['Edit and associate TypeTags'] = 'Επεξεργασία και διασύνδεση TypeTags';
+$lang['Edit typetag'] = 'Επεξεργασία TypeTag';
+$lang['Edited TypeTag'] = 'TypeTag επεξεργάστηκε ';
+$lang['Everywhere'] = 'Παντού';
+$lang['Modify'] = 'Τροποποίηση';
+$lang['New TypeTag'] = 'TypeTag όνομα';
+$lang['New color'] = 'Νέο χρώμα';
+$lang['New name'] = 'Νέο όνομα';
+$lang['Not associated'] = 'Δεν συσχετίζεται';
+$lang['Only on tags page'] = 'Μόνο στη σελίδα ετικετών';
+$lang['typetag_already_exists'] = 'Αυτό το TypeTag υπάρχει ήδη ';
+$lang['typetag_error'] = 'Θα πρέπει να συμπληρώσετε όλα τα πεδία (όνομα και το χρώμα)';
+$lang['typetag_saved'] = 'TypeTag αποθηκεύτηκε ';
+$lang['typetag_suppr'] = 'TypeTag διεγράφη';
+$lang['typetag_unknown'] = 'Άγνωστο TypeTag';
+$lang['typetags_associated'] = 'TypeTags συνδέθηκαν';
+?>
