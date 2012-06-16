9cee3f321c45e6c68e15a65a689a6e42fb037d82
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-16 09:02:21 +0000

    [typetgs] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/typetags@15751 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All associations have been removed'] = 'Összes társítás törölve';
+$lang['Color TypeTag'] = 'Címke típus szín';
+$lang['Create a Typetag'] = 'Címke típus létrehozása';
+$lang['Delete all associations'] = 'Összes társítás törlése';
+$lang['Display colored tags'] = 'Színes címkék megjelenítése';
+$lang['Edit and associate TypeTags'] = 'Címke típus módosítás és társítás';
+$lang['Edit typetag'] = 'Címke típus módosítás';
+$lang['Edited TypeTag'] = 'Módosítandó címke típus';
+$lang['Everywhere'] = 'Mindenütt';
+$lang['Modify'] = 'Módosítás';
+$lang['New TypeTag'] = 'Címke típus neve';
+$lang['New color'] = 'Új szín';
+$lang['New name'] = 'Új név';
+$lang['Not associated'] = 'Nem társított';
+$lang['Only on tags page'] = 'Csak a címkék oldalon';
+$lang['typetag_already_exists'] = 'Ilyes címke típus már létezik';
+$lang['typetag_error'] = 'Ki kel tölteni a mezőket (név és szín)';
+$lang['typetag_saved'] = 'Címke típus mentve';
+$lang['typetag_suppr'] = 'Címke típus törölve';
+$lang['typetag_unknown'] = 'Ismeretlen címke típus';
+$lang['typetags_associated'] = 'Címke típusok társítva';
+?>
