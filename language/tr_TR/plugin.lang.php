fc5ed8a809b301e565cc32d12b165d51d159df32
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-29 16:55:34 +0000

    [typetgs] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/typetags@17195 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['All associations have been removed'] = 'Tüm eşleştirmeler kaldırıldı';
+$lang['Color TypeTag'] = 'TypeTag rengi';
+$lang['Create a Typetag'] = 'TypeTag yarat';
+$lang['Delete all associations'] = 'Tüm eşleştirmeleri sil';
+$lang['Display colored tags'] = 'Renklendirilmiş etiketleri görüntüle';
+$lang['Edit and associate TypeTags'] = 'TypeTag düzenle ve eşleştir';
+$lang['Edit typetag'] = 'TypeTag düzenle';
+$lang['Edited TypeTag'] = 'TypeTag düzenlendi';
+$lang['Everywhere'] = 'Her yerde';
+$lang['Modify'] = 'Değiştir';
+$lang['New TypeTag'] = 'TypeTag ismi';
+$lang['New color'] = 'Yeni renk';
+$lang['New name'] = 'Yeni isim';
+$lang['Not associated'] = 'Eşleştirilmedi';
+$lang['Only on tags page'] = 'Sadece bir etiket sayfası';
+$lang['typetag_already_exists'] = 'Bu TypeTag zaten var';
+$lang['typetag_error'] = 'Tüm alanları doldurmalısınız (isim ve renk)';
+$lang['typetag_saved'] = 'TypeTag kaydedildi';
+$lang['typetag_suppr'] = 'TypeTag silindi';
+$lang['typetag_unknown'] = 'Bilinmeyen TypeTag';
+$lang['typetags_associated'] = 'TypeTag eşleştirildi';
+?>
