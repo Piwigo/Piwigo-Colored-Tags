469b5c0922ee7d214e974f2dd38233b64700a78c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-28 04:43:50 +0000

    [typetgs] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/typetags@18318 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['typetags_associated'] = 'TypeTag tilknyttet';
+$lang['All associations have been removed'] = 'Alle tilknytninger er fjernet';
+$lang['Color TypeTag'] = 'TypeTag-farve';
+$lang['Create a Typetag'] = 'Opret et TypeTag';
+$lang['Delete all associations'] = 'Slet alle tilknytninger';
+$lang['Display colored tags'] = 'Vis farvede tags';
+$lang['Edit and associate TypeTags'] = 'Rediger og tilknyt TypeTags';
+$lang['Edit typetag'] = 'Rediger TypeTag';
+$lang['Edited TypeTag'] = 'TypeTag redigeret';
+$lang['Everywhere'] = 'Overalt';
+$lang['Modify'] = 'Rediger';
+$lang['New TypeTag'] = 'TypeTag-navn';
+$lang['New color'] = 'Ny farve';
+$lang['New name'] = 'Nyt navn';
+$lang['Not associated'] = 'Ikke tilknyttet';
+$lang['Only on tags page'] = 'Kun på tagside';
+$lang['typetag_already_exists'] = 'Dette TypeTag findes allerede';
+$lang['typetag_error'] = 'Du skal udfylde alle felter (navn og farve)';
+$lang['typetag_saved'] = 'TypeTag gemt';
+$lang['typetag_suppr'] = 'TypeTag slettet';
+$lang['typetag_unknown'] = 'Ukendt TypeTag';
+?>
