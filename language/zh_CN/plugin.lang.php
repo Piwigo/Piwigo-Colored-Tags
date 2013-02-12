1352ca8eeb5ed4a6c60468eb3f7615b63f094cfc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-12 22:32:23 +0000

    [typetgs] Add zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/typetags@20726 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Edit and associate TypeTags'] = '编辑和关联TypeTag（标签组）';
+$lang['Not associated'] = '尚未关联到标签组的标签';
+$lang['typetag_error'] = '请完整填写信息（名称与颜色）';
+$lang['typetags_associated'] = 'TypeTag（标签组）已关联';
+$lang['All associations have been removed'] = '所有关联已删除';
+$lang['Color TypeTag'] = 'TypeTag（标签组）颜色';
+$lang['Create a Typetag'] = '创建一个TypeTag（标签组）';
+$lang['Delete all associations'] = '删除所有关联';
+$lang['Display colored tags'] = '将关联了标签组的彩色标签效果显示于：';
+$lang['Edit typetag'] = '编辑TypeTag（标签组）';
+$lang['Edited TypeTag'] = 'TypeTag（标签组）已修改';
+$lang['Everywhere'] = '各处';
+$lang['Modify'] = '修改';
+$lang['New TypeTag'] = 'TypeTag（标签组）名称';
+$lang['New color'] = '新的颜色';
+$lang['New name'] = '新的名称';
+$lang['Only on tags page'] = '仅在标签页';
+$lang['typetag_already_exists'] = '此TypeTag（标签组）已存在';
+$lang['typetag_saved'] = 'TypeTag（标签组）已保存';
+$lang['typetag_suppr'] = 'TypeTag（标签组）已删除';
+$lang['typetag_unknown'] = 'TypeTag（标签组）未知';
+?>
