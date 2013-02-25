2350ac2fbb2a3ee45e680d9f20794df80d0a3001
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-25 17:50:07 +0000

    [typetgs] Add gl_ES, thanks to : damufo
    git-svn-id: http://piwigo.org/svn/extensions/typetags@21031 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/gl_ES/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Color TypeTag'] = 'Color de EtiquetaTipo';
+$lang['Create a Typetag'] = 'Crear EtiquetaTipo';
+$lang['Edit and associate TypeTags'] = 'Modificar e asociar EtiquetasTipo';
+$lang['Edit typetag'] = 'Modificar EtiquetaTipo';
+$lang['Edited TypeTag'] = 'Modificouse a EtiquetaTipo';
+$lang['New TypeTag'] = 'Nome da EtiquetaTipo';
+$lang['typetag_already_exists'] = 'Esta EtiquetaTipo xa existe';
+$lang['typetag_saved'] = 'Gardouse a EtiquetaTipo';
+$lang['typetag_suppr'] = 'Eliminouse a EtiquetaTipo';
+$lang['typetag_unknown'] = 'EtiquetaTipo descoñecida';
+$lang['typetags_associated'] = 'EtiquetasTipo asociadas';
+$lang['typetag_error'] = 'Debes encher tódolos campos (nome e cor)';
+$lang['Delete all associations'] = 'Eliminar todas as asociacións';
+$lang['Display colored tags'] = 'Amosar as etiquetas coloreadas';
+$lang['Everywhere'] = 'Calquera sitio';
+$lang['Modify'] = 'Cambiar';
+$lang['New color'] = 'Nova cor';
+$lang['New name'] = 'Novo nome';
+$lang['Not associated'] = 'Non asociado';
+$lang['Only on tags page'] = 'Só nas páxinas das etiquetas';
+$lang['All associations have been removed'] = 'Elimináronse tódalas asociacións';
+?>
