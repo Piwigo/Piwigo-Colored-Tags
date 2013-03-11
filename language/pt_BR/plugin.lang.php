347c3bfaa7f35e4f0a6fad215b59e056d15dc34c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-11 21:06:26 +0000

    [typetgs] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/typetags@21492 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['typetags_associated'] = 'TipoEtiqueta associada';
+$lang['All associations have been removed'] = 'Todas as associações foram removidos';
+$lang['Color TypeTag'] = 'Cor da TipoEtiqueta';
+$lang['Create a Typetag'] = 'Criar TipoEtiqueta';
+$lang['Delete all associations'] = 'Eliminar todas as associações';
+$lang['Display colored tags'] = 'Exibir etiquetas coloridas';
+$lang['Edit and associate TypeTags'] = 'Editar e associar TipoEtiqueta';
+$lang['Edit typetag'] = 'Editar TipoEtiqueta';
+$lang['Edited TypeTag'] = 'TipoEtiqueta editada';
+$lang['Everywhere'] = 'Em toda parte';
+$lang['Modify'] = 'Modificar';
+$lang['New TypeTag'] = 'Nome da TipoEtiqueta';
+$lang['New color'] = 'Nova cor';
+$lang['New name'] = 'Novo nome';
+$lang['Not associated'] = 'Não está associado';
+$lang['Only on tags page'] = 'Somente na página etiquetas';
+$lang['typetag_already_exists'] = 'Este TipoEtiqueta já existe';
+$lang['typetag_error'] = 'Você deve preencher todos os campos (nome e cor)';
+$lang['typetag_saved'] = 'TipoEtiqueta salvo';
+$lang['typetag_suppr'] = 'TipoEtiqueta eliminado';
+$lang['typetag_unknown'] = 'TipoEtiqueta desconhecido';
+?>
