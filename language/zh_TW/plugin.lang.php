2fc343a775007f90f4c0dd280943948c5dd9d128
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-16 19:28:09 +0000

    [typetgs] Add zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/typetags@24481 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,44 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['typetags_associated'] = 'TypeTag（標籤組）已關聯';
+$lang['New TypeTag'] = 'TypeTag（標籤組）名稱';
+$lang['New color'] = '新的顏色';
+$lang['New name'] = '新的名稱';
+$lang['Not associated'] = '尚未關聯到標籤組的標籤';
+$lang['Only on tags page'] = '僅在標籤頁';
+$lang['typetag_already_exists'] = '此TypeTag（標籤組）已存在';
+$lang['typetag_error'] = '請完整填寫信息（名稱與顏色）';
+$lang['typetag_saved'] = 'TypeTag（標籤組）已保存';
+$lang['typetag_suppr'] = 'TypeTag（標籤組）已刪除';
+$lang['typetag_unknown'] = 'TypeTag（標籤組）未知';
+$lang['All associations have been removed'] = '所有關聯已刪除';
+$lang['Color TypeTag'] = 'TypeTag（標籤組）顏色';
+$lang['Create a Typetag'] = '創建一個TypeTag（標籤組）';
+$lang['Delete all associations'] = '刪除所有關聯';
+$lang['Display colored tags'] = '將關聯了標籤組的彩色標籤效果顯示於：';
+$lang['Edit and associate TypeTags'] = '編輯和關聯TypeTag（標籤組）';
+$lang['Edit typetag'] = '編輯TypeTag（標籤組）';
+$lang['Edited TypeTag'] = 'TypeTag（標籤組）已修改';
+$lang['Everywhere'] = '各處';
+$lang['Modify'] = '修改';
+?>
