c1a2fde27ba503947db321f2df9144928341840c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-13 18:57:29 +0000

    [typetgs] Update it_IT, thanks to : Ericnet
    git-svn-id: http://piwigo.org/svn/extensions/typetags@26685 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -1,32 +1,29 @@
 <?php
 
 /* section */
-$lang['Create a Typetag'] = 'Creare un TypeTag';
-$lang['Edit typetag'] = 'Modificare i TypeTag';
 
 /* fields */
-$lang['Edited TypeTag'] = 'TypeTag modificato';
 $lang['New color'] = 'Nuovo colore';
-$lang['New TypeTag'] = 'Nome del TypeTag';
-$lang['New name'] = 'Nuovo nome';
-$lang['Color TypeTag'] = 'Colore del TypeTag';
 
 /* buttons */
-$lang['Modify'] = 'Modificare';
-$lang['Delete all associations'] = 'Elimina tutte le associazioni';
 
 /* messages */
-$lang['typetag_already_exists'] = 'Questo TypeTag esiste già';
-$lang['typetag_saved'] = 'TypeTag registrato';
-$lang['typetag_suppr'] = 'TypeTag cancellato';
-$lang['typetag_unknown'] = 'TypeTag sconosciuto';
 $lang['You must fill all fields (name and color)'] = 'Dovete inserire tutti campi (nome e colore)';
-$lang['All associations have been removed'] = 'Tutte le associazioni sono state rimosse';
-$lang['typetags_associated'] = 'TypeTag associato';
 
 $lang['Display colored tags'] = 'Mostra tag colorati';
-$lang['Edit and associate TypeTags'] = 'Modifica e associa i TypeTags';
 $lang['Everywhere'] = 'Dovunque';
-$lang['Not associated'] = 'Non associato';
 $lang['Only on tags page'] = 'Solo sulla pagina dei tag';
+$lang['Set a different color for each tag'] = 'Imposta un colore diverso per ogni colore';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Vai a <a href="%s">Photos/Tags</a> per gestire le associazioni';
+$lang['Apply the same color to all tags'] = 'Applica lo stesso colore a tutti i tags';
+$lang['This name is already used'] = 'Questo nome è già in uso';
+$lang['Set tags color'] = 'Imposta tags colore';
+$lang['Invalid color'] = 'Colore non valido';
+$lang['Manage colors'] = 'Gestisci colori';
+$lang['Color saved'] = 'Colore salvato';
+$lang['Edit color'] = 'Modifica colore';
+$lang['Color'] = 'Colore';
+$lang['Color added'] = 'Colore aggiunto';
+$lang['Color deleted'] = 'Colore cancellato';
+$lang['Add a new color'] = 'Aggiungi nuovo colore';
 ?>
