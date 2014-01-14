b7ef7b0a045bc8193f5bdd3cc66772616b6f5cdc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-14 06:14:38 +0000

    [typetgs] Update zh_TW, thanks to : azq1231
    git-svn-id: http://piwigo.org/svn/extensions/typetags@26701 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_TW/plugin.lang.php
+++ language/zh_TW/plugin.lang.php
@@ -20,25 +20,22 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['typetags_associated'] = 'TypeTag（標籤組）已關聯';
-$lang['New TypeTag'] = 'TypeTag（標籤組）名稱';
 $lang['New color'] = '新的顏色';
-$lang['New name'] = '新的名稱';
-$lang['Not associated'] = '尚未關聯到標籤組的標籤';
 $lang['Only on tags page'] = '僅在標籤頁';
-$lang['typetag_already_exists'] = '此TypeTag（標籤組）已存在';
 $lang['You must fill all fields (name and color)'] = '請完整填寫信息（名稱與顏色）';
-$lang['typetag_saved'] = 'TypeTag（標籤組）已保存';
-$lang['typetag_suppr'] = 'TypeTag（標籤組）已刪除';
-$lang['typetag_unknown'] = 'TypeTag（標籤組）未知';
-$lang['All associations have been removed'] = '所有關聯已刪除';
-$lang['Color TypeTag'] = 'TypeTag（標籤組）顏色';
-$lang['Create a Typetag'] = '創建一個TypeTag（標籤組）';
-$lang['Delete all associations'] = '刪除所有關聯';
 $lang['Display colored tags'] = '將關聯了標籤組的彩色標籤效果顯示於：';
-$lang['Edit and associate TypeTags'] = '編輯和關聯TypeTag（標籤組）';
-$lang['Edit typetag'] = '編輯TypeTag（標籤組）';
-$lang['Edited TypeTag'] = 'TypeTag（標籤組）已修改';
 $lang['Everywhere'] = '各處';
-$lang['Modify'] = '修改';
+$lang['Add a new color'] = '添加一個新的顏色';
+$lang['Apply the same color to all tags'] = '所有標籤應用相同的顏色';
+$lang['Color'] = '顏色';
+$lang['Color added'] = '增加顏色';
+$lang['Color deleted'] = '刪除顏色';
+$lang['Color saved'] = '保存顏色';
+$lang['Edit color'] = '編輯顏色';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = '轉到<a href="%s">照片/標籤 </a> 以管理關聯。';
+$lang['Invalid color'] = '無效的顏色';
+$lang['Manage colors'] = '色彩管理';
+$lang['Set a different color for each tag'] = '為每個標籤設置不同的顏色';
+$lang['Set tags color'] = '設置標籤的顏色';
+$lang['This name is already used'] = '此名稱已被使用';
 ?>
