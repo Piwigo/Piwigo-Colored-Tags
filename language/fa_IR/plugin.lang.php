c52fc824f1a48fb8f3af2fed707aa2d195dddc29
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-14 20:23:09 +0000

    [typetgs] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/extensions/typetags@26710 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a new color'] = 'افزودن رنگ جدید';
+$lang['Apply the same color to all tags'] = 'بیفزای  تعدادی رنگ برای کل تگ ها';
+$lang['Color'] = 'رنگ';
+$lang['Color added'] = 'رنگ افزوده شد';
+$lang['Color deleted'] = 'رنگ پاک شد';
+$lang['Color saved'] = 'رنگ ذخیره شد';
+$lang['Display colored tags'] = 'نمایش رنگ برچسب ها';
+$lang['Edit color'] = 'ویرایش رنگ';
+$lang['Everywhere'] = 'هر جایی';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'رفتن به<a href="%s">Photos/Tags</a> مدیریت پیوند ها';
+$lang['Invalid color'] = 'رنگ ناموجود است';
+$lang['Manage colors'] = 'مدیریت رنگ ها';
+$lang['New color'] = 'رنگ جدید';
+$lang['Only on tags page'] = 'فقط برروی برچسب های  صفحه';
+$lang['Set a different color for each tag'] = '
+تنظیم رنگ های مختلف برای هر تگ';
+$lang['Set tags color'] = 'رنگ برچسب تنظیم شد ';
+$lang['This name is already used'] = 'این نام مورد استفاده و موجود است';
+$lang['You must fill all fields (name and color)'] = 'شما باید همه قسمتها را پر کنید (نام و رنگ)';
+?>
