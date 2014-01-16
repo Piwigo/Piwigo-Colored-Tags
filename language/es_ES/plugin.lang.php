d263bf712d3aedd00ea881f9dd5c9c39557fcc7e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-16 09:38:12 +0000

    [typetgs] Update es_ES, thanks to : jpr928
    git-svn-id: http://piwigo.org/svn/extensions/typetags@26801 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -1,32 +1,29 @@
 <?php
 
 /* section */
-$lang['Create a Typetag'] = 'Crear TypeTag';
-$lang['Edit typetag'] = 'Editar TypeTag';
 
 /* fields */
-$lang['Edited TypeTag'] = 'Typetag editado';
 $lang['New color'] = 'Nuevo color';
-$lang['New TypeTag'] = 'Nombre de TypeTag';
-$lang['New name'] = 'Nuevo nombre';
-$lang['Color TypeTag'] = 'Color de TypeTag';
 
 /* buttons */
-$lang['Modify'] = 'Cambiar';
-$lang['Delete all associations'] = 'Delete all associations';
 
 /* messages */
-$lang['typetag_already_exists'] = 'Este TypeTag ya existe';
-$lang['typetag_saved'] = 'TypeTag registrado';
-$lang['typetag_suppr'] = 'TypeTag suprimido';
-$lang['typetag_unknown'] = 'Unknown TypeTag';
 $lang['You must fill all fields (name and color)'] = 'Usted debe informar todos los campos (nombre y color)';
-$lang['All associations have been removed'] = 'All associations have been removed';
-$lang['typetags_associated'] = 'TypeTags asociado';
 
 $lang['Display colored tags'] = 'Mostrar etiquetas coloreadas';
-$lang['Edit and associate TypeTags'] = 'Editar y asociar TypeTags';
 $lang['Everywhere'] = 'Cualquier sitio';
-$lang['Not associated'] = 'No asociado';
 $lang['Only on tags page'] = 'Solo en las páginas de las etiquetas';
+$lang['Add a new color'] = 'Añadir nuevo color';
+$lang['Apply the same color to all tags'] = 'Aplicar el mismo color a todas las etiquetas ';
+$lang['Color'] = 'Color';
+$lang['Color added'] = 'Color añadidos';
+$lang['Color deleted'] = 'Color eliminados';
+$lang['Color saved'] = 'Color guardado';
+$lang['Edit color'] = 'Editar color';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Ir a <a href="%s">Fotos/Etiquetas</a> para gestionar las asociaciones';
+$lang['Invalid color'] = 'Color invalido';
+$lang['Manage colors'] = 'Gestionar colores';
+$lang['Set a different color for each tag'] = 'Elija un color diferente por cada etiquetas';
+$lang['Set tags color'] = 'Elija un color para las etiquetas';
+$lang['This name is already used'] = 'Este nombre ya esta utilizado';
 ?>
