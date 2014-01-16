2fff24acf1ff82bd1e3081a23c8c111abae90beb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-16 18:18:54 +0000

    [typetgs] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/typetags@26812 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -20,25 +20,22 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Edit and associate TypeTags'] = '编辑和关联TypeTag（标签组）';
-$lang['Not associated'] = '尚未关联到标签组的标签';
 $lang['You must fill all fields (name and color)'] = '请完整填写信息（名称与颜色）';
-$lang['typetags_associated'] = 'TypeTag（标签组）已关联';
-$lang['All associations have been removed'] = '所有关联已删除';
-$lang['Color TypeTag'] = 'TypeTag（标签组）颜色';
-$lang['Create a Typetag'] = '创建一个TypeTag（标签组）';
-$lang['Delete all associations'] = '删除所有关联';
-$lang['Display colored tags'] = '将关联了标签组的彩色标签效果显示于：';
-$lang['Edit typetag'] = '编辑TypeTag（标签组）';
-$lang['Edited TypeTag'] = 'TypeTag（标签组）已修改';
+$lang['Display colored tags'] = '将彩色标签效果显示于：';
 $lang['Everywhere'] = '各处';
-$lang['Modify'] = '修改';
-$lang['New TypeTag'] = 'TypeTag（标签组）名称';
-$lang['New color'] = '新的颜色';
-$lang['New name'] = '新的名称';
+$lang['New color'] = '新颜色';
 $lang['Only on tags page'] = '仅在标签页';
-$lang['typetag_already_exists'] = '此TypeTag（标签组）已存在';
-$lang['typetag_saved'] = 'TypeTag（标签组）已保存';
-$lang['typetag_suppr'] = 'TypeTag（标签组）已删除';
-$lang['typetag_unknown'] = 'TypeTag（标签组）未知';
+$lang['Add a new color'] = '添加新颜色';
+$lang['Apply the same color to all tags'] = '对所有标签应用相同的颜色';
+$lang['Color'] = '颜色';
+$lang['Color added'] = '颜色已添加';
+$lang['Color deleted'] = '颜色已删除';
+$lang['Color saved'] = '颜色已保存';
+$lang['Edit color'] = '编辑颜色';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = '前往<a href="%s">Photos/Tags</a>以管理关联。';
+$lang['Invalid color'] = '颜色无效';
+$lang['Manage colors'] = '管理颜色';
+$lang['Set a different color for each tag'] = '为每个标签设置独立的颜色';
+$lang['Set tags color'] = '设置标签颜色';
+$lang['This name is already used'] = '此名称已存在';
 ?>
