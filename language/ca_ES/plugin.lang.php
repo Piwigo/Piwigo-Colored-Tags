840dbffc31e9aece4cb55e310136d006ba2bf6f2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-10 21:53:30 +0000

    [typetgs] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/extensions/typetags@27318 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ca_ES/plugin.lang.php
+++ language/ca_ES/plugin.lang.php
@@ -20,25 +20,10 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Modify'] = 'Modificar';
 $lang['New color'] = 'Nou color';
-$lang['New name'] = 'Nou nom';
-$lang['All associations have been removed'] = 'Totes les associacions has sigut eliminades';
-$lang['Color TypeTag'] = 'Color del TypeTag';
-$lang['Create a Typetag'] = 'Crear un TypeTag';
-$lang['Delete all associations'] = 'Eliminar totes les associacions';
 $lang['Display colored tags'] = 'Visualitzar les etiquetes colorejades';
-$lang['Edit and associate TypeTags'] = 'Editar i associar TypeTags';
-$lang['Edit typetag'] = 'Editar TypeTag';
-$lang['Edited TypeTag'] = 'TypeTag editat';
 $lang['Everywhere'] = 'A tot arreu';
-$lang['New TypeTag'] = 'Nom del TypeTag';
-$lang['Not associated'] = 'No associat';
 $lang['Only on tags page'] = 'Només a la pàgina etiquetes';
-$lang['typetag_already_exists'] = 'Aquest TypeTag ja existeix';
 $lang['You must fill all fields (name and color)'] = 'Cal omplir tots els camps (nom i color)';
-$lang['typetag_saved'] = 'TypeTag desat';
-$lang['typetag_suppr'] = 'TypeTag eliminat';
-$lang['typetag_unknown'] = 'TypeTag desconegut';
-$lang['typetags_associated'] = 'TypeTags associades';
+$lang['Color'] = 'Color';
 ?>
