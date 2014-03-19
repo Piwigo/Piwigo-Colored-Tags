4b213b5cc172914d09bc99bbb90b5471958bca1f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-19 22:44:42 +0000

    [typetgs] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/typetags@27847 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,33 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Display colored tags'] = 'Vidigi koloritajn etikedojn';
+$lang['This name is already used'] = 'La nomo jam uziĝas';
+$lang['New color'] = 'Nova koloro';
+$lang['Add a new color'] = 'Aldoni novan koloron';
+$lang['Invalid color'] = 'Nevalida koloro';
+$lang['Edit color'] = 'Redakti koloron';
+$lang['Color deleted'] = 'Forigita koloro';
+$lang['Color saved'] = 'Konservita koloro';
+$lang['Color added'] = 'Aldonita koloro';
+$lang['Everywhere'] = 'Ĉie';
+$lang['Color'] = 'Koloro';
