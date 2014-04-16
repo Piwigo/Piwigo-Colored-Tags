8da4e0e4c070baf17ac2cba32e147d0156d484bf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-04-16 19:44:33 +0000

    [typetgs] Update sl_SI, thanks to : MarkoAm
    git-svn-id: http://piwigo.org/svn/extensions/typetags@28202 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Pojdite na <a href="%s">Slike/Oznake</a> za upravljanje združevanj.';
+$lang['Invalid color'] = 'Nepravilna barva';
+$lang['Manage colors'] = 'Upravljanje barv';
+$lang['New color'] = 'Nova barva';
+$lang['Only on tags page'] = 'Samo ena oznaka strani';
+$lang['Set a different color for each tag'] = 'Nastavi različno barvo za vsako oznako';
+$lang['Set tags color'] = 'Nastavitev barvnih oznak';
+$lang['This name is already used'] = 'To ime je že v uporabi';
+$lang['You must fill all fields (name and color)'] = 'Izpolniti morate vsa polja (ime in barva)';
+$lang['Add a new color'] = 'Dodaj novo barvo';
+$lang['Apply the same color to all tags'] = 'Uporabi enako barvno za vse oznake';
+$lang['Color'] = 'Barva';
+$lang['Color added'] = 'Barva dodana';
+$lang['Color deleted'] = 'Barva izbrisana';
+$lang['Color saved'] = 'Barva shranjena';
+$lang['Display colored tags'] = 'Oznake prikaži v barvah';
+$lang['Edit color'] = 'Uredi barvo';
+$lang['Everywhere'] = 'Povsod';
