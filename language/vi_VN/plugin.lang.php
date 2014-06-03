504a589e220f197438f49617c519e2d18bb9b8b6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-03 04:23:46 +0000

    [typetgs] Update vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/typetags@28614 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a new color'] = ' Thêm màu mới';
+$lang['Apply the same color to all tags'] = 'Áp dụng cùng màu cho tất cả tag';
+$lang['Color'] = 'Màu';
+$lang['Color added'] = 'Màu đã được thêm';
+$lang['Color deleted'] = 'Màu đã được xóa';
+$lang['Color saved'] = 'Màu đã được lưu';
+$lang['Display colored tags'] = 'Hiện các tag màu';
+$lang['Edit color'] = 'Sửa màu';
+$lang['Everywhere'] = 'Mọi nơi';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Đi đến <a href="%s">Photos/Tags</a> để quan lý các liên kết.';
+$lang['Invalid color'] = 'Màu không hợp lệ';
+$lang['Manage colors'] = 'Quản lý màu sắc';
+$lang['New color'] = 'Màu mới';
+$lang['Only on tags page'] = 'Chỉ trong trang tag';
+$lang['Set a different color for each tag'] = 'Đặt màu khác cho mỗi tag';
+$lang['Set tags color'] = 'Đặt màu tag';
+$lang['This name is already used'] = 'Tên này đã đươc sử dụng';
+$lang['You must fill all fields (name and color)'] = 'Bạn phải điền tất cả các trường (tên và màu)';
