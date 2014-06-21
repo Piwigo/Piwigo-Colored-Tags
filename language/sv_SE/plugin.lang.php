14c2762db5f15f5dc1a12ffbb4efee916e56feb4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-06-21 05:31:08 +0000

    [typetgs] Update sv_SE, thanks to : Blytung
    git-svn-id: http://piwigo.org/svn/extensions/typetags@28752 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a new color'] = 'Lägg till ny färg';
+$lang['Apply the same color to all tags'] = 'Använd samma färg till alla taggar';
+$lang['Color'] = 'Färg';
+$lang['Color added'] = 'Färg tillagd';
+$lang['Color deleted'] = 'Färg borttagen';
+$lang['Color saved'] = 'Färg sparad';
+$lang['Display colored tags'] = 'Visa färgade taggar';
+$lang['Edit color'] = 'Editera färg';
+$lang['Everywhere'] = 'Överallt';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Gå till <a href="%s">Photos/Tags</a>för att ställa in associationer.';
+$lang['Invalid color'] = 'Ogiltig färg';
+$lang['Manage colors'] = 'Administrera färger';
+$lang['New color'] = 'Ny färg';
+$lang['Only on tags page'] = 'Endast på "Taggar-sidan"';
+$lang['Set a different color for each tag'] = 'Välj olika färg för varje tag';
+$lang['Set tags color'] = 'Välj taggfärger';
+$lang['This name is already used'] = 'Namnet är redan upptaget';
+$lang['You must fill all fields (name and color)'] = 'Du måste fylla i alla fält (namn och färg)';
