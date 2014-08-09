f4f7b0a9a2c14c4b56260062cc0f406becd25392
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-08-09 20:32:51 +0000

    [typetgs] Update bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/typetags@29189 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You must fill all fields (name and color)'] = 'Трябва да попълните всички полета (име и цвят)';
+$lang['This name is already used'] = 'Това име вече е заето';
+$lang['Set tags color'] = 'Задай цвят на таговете';
+$lang['Set a different color for each tag'] = 'Задавай различен цвят за всеки таг';
+$lang['Only on tags page'] = 'Само на страницата с тагове';
+$lang['New color'] = 'Нов цвят';
+$lang['Manage colors'] = 'Редакция на цветове';
+$lang['Invalid color'] = 'Невалиден цвят';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Отиди на <a href="%s">Снимки/Тагове</a> за редакция на асоциациите.';
+$lang['Everywhere'] = 'Навсякъде';
+$lang['Edit color'] = 'Редакция на цвят';
+$lang['Display colored tags'] = 'Показване на цветни тагове';
+$lang['Color saved'] = 'Цветът е записан';
+$lang['Color added'] = 'Цветът е добавен ';
+$lang['Color deleted'] = 'Цветът е изтрит';
+$lang['Color'] = 'Цвят';
+$lang['Apply the same color to all tags'] = 'Задай цвета за всички тагове';
+$lang['Add a new color'] = 'Добави нов цвят';
