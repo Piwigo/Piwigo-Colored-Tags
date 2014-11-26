3cf39e5676fc2899fe36fe8643d769cefac09347
Piwigo-TranslationTeam <Piwigo-TranslationTeam@users.noreply.github.com>
2014-11-26 07:10:07 +0100

    [typetgs] Update de_DE, thanks to bigant

--- language/de_DE/plugin.lang.php
+++ language/de_DE/plugin.lang.php
@@ -10,9 +10,9 @@ $lang['New color'] = 'Neue Farbe';
 /* messages */
 $lang['You must fill all fields (name and color)'] = 'Alle Felder müssen ausgefüllt werden (Name und Farbe)';
 
-$lang['Display colored tags'] = 'Zeige farbige Tags';
+$lang['Display colored tags'] = 'Farbige Tags anzeigen';
 $lang['Everywhere'] = 'Überall';
-$lang['Only on tags page'] = 'Nur auf Tags Seiten';
+$lang['Only on tags page'] = 'Nur auf Tags-Seite';
 $lang['Add a new color'] = 'Eine neue Farbe hinzufügen';
 $lang['Apply the same color to all tags'] = 'Dieselbe Farbe auf alle Tags anwenden';
 $lang['Color'] = 'Farbe';
@@ -20,10 +20,9 @@ $lang['Color added'] = 'Farbe hinzugefügt';
 $lang['Color deleted'] = 'Farbe entfernt';
 $lang['Color saved'] = 'Farbe gespeichert';
 $lang['Edit color'] = 'Farbe editieren';
-$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Besuche <a href="%s">Photos/Tags</a>, um Zuweisungen zu verwalten.';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Gehe zu<a href="%s">Photos/Tags</a>, um Zuweisungen zu verwalten.';
 $lang['Invalid color'] = 'Ungültige Farbe';
 $lang['Manage colors'] = 'Farben verwalten';
 $lang['Set a different color for each tag'] = 'Setze eine unterschiedliche Farbe für jeden Tag';
-$lang['Set tags color'] = 'Setze Tag-farben';
+$lang['Set tags color'] = 'Tag-Farben festlegen';
 $lang['This name is already used'] = 'Dieser Name ist bereits in Verwendung';
-?>
