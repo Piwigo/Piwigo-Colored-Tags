ffa6cec19821942bee58cdd03d3af441268e363f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-06-18 11:01:14 +0200

    Update nb_NO, thanks to paulen

--- language/nb_NO/plugin.lang.php
+++ language/nb_NO/plugin.lang.php
@@ -18,7 +18,7 @@ $lang['Set a different color for each tag'] = 'Lagre forskjellig farge for hver 
 $lang['Set tags color'] = 'Lagre tags farge';
 $lang['This name is already used'] = 'Dette navn er allerede valgt';
 $lang['Invalid color'] = 'Ugyldig farge';
-$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Gå til <a href="%s">Bilde/Tags</a>';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Gå til <a href="%s">Bilder/Tags</a> for å administrere tilknytninger.';
 $lang['Edit color'] = 'Edit farge';
 $lang['Color saved'] = 'Farge lagret';
 $lang['Color deleted'] = 'Farge slettet';
@@ -26,3 +26,4 @@ $lang['Color added'] = 'Farge lagt til';
 $lang['Color'] = 'Farge';
 $lang['Apply the same color to all tags'] = 'Tilføy samme farge til alle tags';
 $lang['Add a new color'] = 'Legg til en ny farge';
+$lang['Go to <a href="%s">plugin page</a> to edit colors'] = 'Gå til <a href="%s">plugin page</a> for å redigere farger';
