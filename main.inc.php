dace2d7561ce45967b6a2626d38e9692c906d2e1
Matthieu Leproux <leproux.matthieu@gmail.com>
2021-11-22 16:18:18 +0100

    Compatibility for piwigo 12

--- main.inc.php
+++ main.inc.php
@@ -6,6 +6,7 @@ Description: Allow to manage color of tags, as you want...
 Plugin URI: auto
 Author: Mistic
 Author URI: http://www.strangeplanet.fr
+Has Settings: true
 */
 
 defined('PHPWG_ROOT_PATH') or die('Hacking attempt!');
