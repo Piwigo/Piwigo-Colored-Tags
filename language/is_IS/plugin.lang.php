3b27f2de5eb3dc18a51e34f775fb821a43fcbb06
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-10 07:56:24 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add a new color'] = 'Bættu við nýjum lit';
+$lang['Apply the same color to all tags'] = 'Notaðu sama lit á öll merki';
+$lang['Color'] = 'Litur';
+$lang['Color added'] = 'Liti bætt við';
+$lang['Color deleted'] = 'Liti eytt';
+$lang['Color saved'] = 'Litur vistaður';
+$lang['Display colored tags'] = 'Birta lituð merki';
+$lang['Edit color'] = 'Breyta lit';
+$lang['Everywhere'] = 'Alls staðar';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'Farðu í <a href="%s">Myndir/Merki</a> til að stjórna tengingum.';
+$lang['Go to <a href="%s">plugin page</a> to edit colors'] = 'Farðu í <a href="%s">síðuna Viðbætur</a> til að breyta litum';
+$lang['Invalid color'] = 'Ógildur litur';
+$lang['Manage colors'] = 'Stjórna litum';
+$lang['New color'] = 'Nýr litur';
+$lang['Only on tags page'] = 'Aðeins á merkjasíðu';
+$lang['Set a different color for each tag'] = 'Settu mismunandi lit á hvert merki';
+$lang['Set tags color'] = 'Settu lit á merki';
+$lang['This name is already used'] = 'Þetta heitir er þegar í notkun';
+$lang['You must fill all fields (name and color)'] = 'Þú verður að fylla út í alla reiti (heiti og litur)';
