0e4ed498d180f0bd0bd62cd1085c39375550f419
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:54:52 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,26 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add a new color'] = 'הוסף צבע חדש';
+$lang['Apply the same color to all tags'] = 'החל את אותו הצבע לכל התגיות';
+$lang['Color'] = 'צבע';
+$lang['Color added'] = 'צבע נוסף';
+$lang['Color deleted'] = 'צבע נמחק';
+$lang['Color saved'] = 'צבע נשמר';
+$lang['Display colored tags'] = 'הצג תגיות מצובעות';
+$lang['Edit color'] = 'ערוך צבע';
+$lang['Everywhere'] = 'בכל מקום';
+$lang['Go to <a href="%s">Photos/Tags</a> to manage associations.'] = 'עבור <a href="%s">תמונות/תגיות</a> לנהל אילוצים.';
+$lang['Go to <a href="%s">plugin page</a> to edit colors'] = 'עבור <a href="%s">דף התוסף</a> לעריכת צבעים';
+$lang['Invalid color'] = 'צבע לא חוקי';
+$lang['Manage colors'] = 'נהל צבעים';
+$lang['New color'] = 'צבע חדש';
+$lang['Only on tags page'] = 'רק בדף התגיות';
+$lang['Set a different color for each tag'] = 'הגדר צבע שונה לכל תג';
+$lang['Set tags color'] = 'הגדר צבעי תגים';
+$lang['This name is already used'] = 'שם זה כבר בשימוש';
+$lang['You must fill all fields (name and color)'] = 'עליך למלא את כל השדות (שם וצבע)';
